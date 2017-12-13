294b36e8f5d163c0abe47ad194b2c090b326ba26
TomSteer <tom.steer@designagency.co.uk>
2017-12-13 15:51:30 +0000

    #1 WIP Improve package build process

---
+++ build-appveyor.cmd
@@ -0,0 +1,8 @@
+ECHO APPVEYOR_REPO_BRANCH: %APPVEYOR_REPO_BRANCH%
+ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
+ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
+ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
+
+CALL build\Tools\NuGet.exe restore development\DesignAgency.BoboFacets.sln
+CALL "%programfiles(x86)%\MSBuild\14.0\Bin\amd64\MsBuild.exe" build\package.build.xml
+pause
