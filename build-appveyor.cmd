67e2c8b6ac6458ba8a83c51078182deb71dd741c
TomSteer <tom.steer@designagency.co.uk>
2017-12-14 16:33:20 +0000

    Update build script

--- build-appveyor.cmd
+++ build-appveyor.cmd
@@ -4,5 +4,5 @@ ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 
 CALL build\Tools\NuGet.exe restore development\DesignAgency.BoboFacets.sln
-CALL "%programfiles(x86)%\MSBuild\14.0\Bin\amd64\MsBuild.exe" build\package.build.xml
+CALL "%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64\MsBuild.exe" build\package.build.xml
 pause
