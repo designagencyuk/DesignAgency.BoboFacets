472eb1905c4cf50b3df969476c2517caafccab9a
TomSteer <tom.steer@designagency.co.uk>
2018-07-23 12:45:12 +0100

    Release 0.1.1

--- development/DesignAgency.BoboFacets/Properties/VersionInfo.cs
+++ development/DesignAgency.BoboFacets/Properties/VersionInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.1")]
 
 
